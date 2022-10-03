82219e4b9bf805193c092c8dc20b8c34101ec24d
Sebastien Coutu <sebastien@metacommerce.com>
2022-10-03 15:57:18 -0400

    chore: add scripts to source for all the right variables

---
+++ deployment/deploy-to-staging.sh
@@ -0,0 +1,8 @@
+export AWS_PROFILE=staging
+export AWS_SDK_LOAD_CONFIG=1
+export AWS_REGION=us-east-2
+export REGION=us-east-2 # the AWS region to launch the solution (e.g. us-east-1)
+export SECONDARY_REGION=us-west-2 # the AWS region that will serve as backup (e.g. eu-central-1)
+export DIST_OUTPUT_BUCKET=mc-staging-cognito-user-profiles-export # bucket where customized code will reside
+export SOLUTION_NAME=cognito-user-profiles-export
+export VERSION=1.0.1 # version number for the customized code
